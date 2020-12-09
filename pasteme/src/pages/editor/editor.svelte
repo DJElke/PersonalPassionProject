df0f4c223f7e5fdf11b6b699399ea408ea79036d
elkedejonckere <elkedejonckere@gmail.com>
2020-12-09 16:48:57 +0100

    Add: Camera button to editor

--- pasteme/src/pages/editor/editor.svelte
+++ pasteme/src/pages/editor/editor.svelte
@@ -14,19 +14,21 @@
             video.captureStream = () => stream;
         }
 
-        console.log(video);
-
         video.play();
     });
 
 </script>
 
 <main class="camera">
+    <!-- show the camera output -->
     <video class="camera__view" bind:this={video} width={width} height={height} playsinline autoplay muted></video>
+
+    <!-- button to take a picture -->
+    <button class="camera__trigger"><img class="camera__icon" src="/icons/camera-main.svg" alt="camera"/></button>
 </main>
 
 <style>
-    .camera{
+    .camera, .camera__view{
         position: fixed;
         object-fit: cover;
     }
@@ -34,7 +36,31 @@
     .camera__view{
         transform: scaleX(-1);
         filter: FlipH;
-        object-fit: cover;
+    }
+
+    .camera__trigger{
+        position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
+        bottom: calc(100px - (80px/1));
+        margin-bottom: 0;
+        height: 80px;
+        width: 80px;
+        border-radius: 40px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        background-color: white;
+        z-index: 9;
+        transition: height 0.3s;
+        box-shadow: 0 5px 10px 0 rgba(0,0,0,0.2);
+    }
+
+    .camera__icon{
+        width: 75%;
+        height: auto;
     }
 </style>
 
